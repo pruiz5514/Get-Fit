dae3eaac5cbfc5fadfb7623a64c0f6f913f4162e
Pablo Ruiz <pablo_ruiza@hotmail.com>
2025-03-03 12:08:43 -0500

    redux configuration is done

---
+++ src/redux/features/authSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface AuthState {
+    user: string | null;
+    isAuthenticated: boolean;
+}
+  
+const initialState: AuthState = {
+    user: null,
+    isAuthenticated: false,
+};
+
+export const authSlice = createSlice({
+    name:'auth',
+    initialState,
+    reducers:{
+        login: (state, action: PayloadAction<string>) => {
+            state.user = action.payload;
+            state.isAuthenticated = true
+        },
+        logout: (state) =>{
+            state.user = null;
+            state.isAuthenticated = false;
+            localStorage.removeItem("persist:root");
+        }
+    }
+})
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer
---
+++ src/redux/store.ts
@@ -0,0 +1,23 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage"; 
+import { persistReducer, persistStore } from "redux-persist";
+import authReducer from './features/authSlice'
+
+const persistConfig = {
+    key: "root",
+    storage,
+};
+
+const rootReducer = combineReducers({
+    auth: persistReducer(persistConfig, authReducer),
+});
+
+export const store = configureStore({
+    reducer: {
+        rootReducer
+    }
+})
+
+export const persistor = persistStore(store);
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
